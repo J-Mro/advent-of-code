2351045f6d5ca4b497f80e3a277297549c707348
Joseph Mroueh <j.k.mroueh@gmail.com>
2026-06-18 09:11:55 +0100

    refactor solution for part 1, remove parseInstruction and turnDial functions

--- 2025/go-solution/day1/day1.go
+++ 2025/go-solution/day1/day1.go
@@ -11,34 +11,12 @@ import (
 	"log"
 	"os"
 	"strconv"
-	"strings"
 )
-
-func parseInstruction(direction string) int {
-	normalised := strings.ToLower(direction)
-	sign := ""
-	if normalised[0] == 'l' {
-		sign += "-"
-	} else if normalised[0] == 'r' {
-		sign += "+"
-	}
-	convertedDirection := sign + direction[1:]
-	parsed, err := strconv.Atoi(convertedDirection)
-	if err != nil {
-		log.Fatal("something went wrong:", err)
-	}
-	return parsed
-}
-
-func turnDial(dialPosition, instruction int) int {
-	sum := dialPosition + instruction
-
-	return ((sum % 100) + 100) % 100
-}
 
 func main() {
 	dialStart := 50
 	numZeroes := 0
+
 	filePath := "input.txt" // Relative path
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -46,7 +24,6 @@ func main() {
 	}
 	defer file.Close()
 
-	fmt.Println("Reading file line by line:")
 	scanner := bufio.NewScanner(file) // method to read a file one line at a time
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -54,14 +31,32 @@ func main() {
 
 	for scanner.Scan() { // advances the scanner to the next line
 		line := scanner.Text()
-		parsed := parseInstruction(line)
-		dialStart = turnDial(dialStart, parsed)
-		// fmt.Println("current position: ", dialStart)
+		direction := line[0]
+		distance, err := strconv.Atoi(line[1:])
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(line)
 
+		for range distance {
+			if direction == 'L' {
+				if dialStart == 0 {
+					numZeroes++
+					dialStart = 100
+				}
+				dialStart--
+			} else {
+				dialStart++
+				if dialStart == 100 {
+					numZeroes++
+					dialStart = 0
+				}
+			}
+		}
 		if dialStart == 0 {
 			numZeroes++
 		}
+
 	}
 	fmt.Println(numZeroes)
-
 }
