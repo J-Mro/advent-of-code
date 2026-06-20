93e1941612b29cae72099940c1ad0ad8b60cd9c2
Joseph Mroueh <j.k.mroueh@gmail.com>
2026-06-20 23:19:37 +0100

    refactor main to include part 2 solution. This changes logic to use a new slice check method

--- 2025/go-solution/day2/day2.go
+++ 2025/go-solution/day2/day2.go
@@ -13,6 +13,31 @@ import (
 // e.g. 55, 6464, 123123 are INVALID
 // no numbers have leading zeroes (0101 is invalid, 101 is valid)
 // find all invalid ids in given range
+
+// for part 2:
+// many odd numbers are not prime therefore cannot assume all odd length ids can only have a repeated pattern of 0000000 (for example)
+// e.g. you could have an id length of 15 that could be something like 135135135135135
+
+// Old part 2 logic
+func isSameDigitRepeated(idString string, idNum int, idLength int, sumInvalid int) bool {
+	numSameDigit := 0
+	l := 0
+	for range idLength {
+		// The repeating string for an invalid id can repeat at least twice
+		if idString[l] == idString[0] {
+			numSameDigit++
+		}
+		l++
+	}
+
+	if numSameDigit == idLength {
+		fmt.Println(idNum)
+		sumInvalid += idNum
+		return true
+	}
+	return false
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -43,11 +68,25 @@ func main() {
 			for j := start; j <= end; j++ {
 				idString := strconv.Itoa(j)
 				idLength := len(idString)
-				if idLength%2 == 0 {
-					if idString[0:idLength/2] == idString[idLength/2:] {
-						sumInvalid += j
+
+				// checking for repeating patterns in strings
+				isRepeated := false
+				for i := 1; i < idLength; i++ {
+					substring := strings.Repeat(idString[:i], idLength/i)
+					if idLength%i == 0 && substring == idString {
+						isRepeated = true
 					}
 				}
+				if isRepeated {
+					sumInvalid += j
+				}
+
+				// // Part 1 logic
+				// if idLength%2 == 0 {
+				// 	if idString[0:idLength/2] == idString[idLength/2:] {
+				// 		sumInvalid += j
+				// 	}
+				// }
 			}
 		}
 	}
